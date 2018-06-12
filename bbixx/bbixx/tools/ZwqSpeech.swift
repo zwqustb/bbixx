28f5ac88a64ee35e8b889d04be5d686e7c807459
zwqustb <522976741@qq.com>
2018-06-12 13:11:14 +0800

    allfile

---
+++ bbixx/bbixx/tools/ZwqSpeech.swift
@@ -0,0 +1,17 @@
+//
+//  ZwqSpeech.swift
+//  bbixx
+//
+//  Created by zhangwenqiang on 2018/6/12.
+//  Copyright © 2018年 zhangwenqiang. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+class ZwqSpeech: NSObject {
+    static let `default` = ZwqSpeech()
+    func speakWords()  {
+        
+    }
+
+}
